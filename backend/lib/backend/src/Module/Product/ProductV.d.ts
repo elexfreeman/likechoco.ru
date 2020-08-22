3a57a940a2b106ac09f1a86f11692d9761913857
alex <AAnPolozov@sberbank.ru>
2020-08-22 22:08:47 +0300

    build

--- backend/lib/backend/src/Module/Product/ProductV.d.ts
+++
@@ -1,8 +0,0 @@
-import { MainRequest } from "@a-a-game-studio/aa-core/lib/Namespace/System";
-/**
- * List
- *
- * @param req MainRequest
- * @param data RequestI
- */
-export declare function list(req: MainRequest, data: any): any;
