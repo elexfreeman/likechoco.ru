039ae49aa831fdd59fa54e8f4027c5b36dfb5b40
alex <AAnPolozov@sberbank.ru>
2020-08-25 13:28:49 +0300

    test product tag

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,32 @@ import { App } from "@a-a-game-studio/aa-core/lib";
 import config from "./Config/MainConfig";
 import { MemSysI } from '@a-a-game-studio/aa-redis-sys/lib/CacheSys';
 import { ProductCtrl } from "./Module/Product/ProductCtrl";
+import { ProductTagCtrl } from "./Module/ProductTag/ProductTagCtrl";
+import { ProductCategoryCtrl } from "./Module/ProductCategory/ProductCategoryCtrl";
 const mem: MemSysI = {}
 
 class MyApp extends App {
-    public fUseProduct(): App {
-        console.log('Use product');
 
+    public fUseProduct(): App {
+        console.log('+ Use product');
         this.objExpress.use(ProductCtrl)
+        return this;
+    }
+    public fUseProductTag(): App {
+        console.log('+ Use product tag');
+        this.objExpress.use(ProductTagCtrl)
+        return this;
+    }
+    public fUseProductCategory(): App {
+        console.log('+ Use product category');
+        this.objExpress.use(ProductCategoryCtrl)
         return this;
     }
 
     public fUseLoger(): App {
         this.objExpress.use((req, resp, next) => {
             console.log(req.headers.token);
-            
+
             console.log(req.originalUrl);
             next();
         });
@@ -25,11 +37,16 @@ class MyApp extends App {
 }
 
 const app: MyApp = new MyApp(config);
+
 app.fDisableCors(); // отключаем cors
 app.fUseBodyParser(); // используем дефолтный BodyParser
 app.fUseMySql();
 app.fUseSharedMem(mem);
 app.faUseAuthSys()
 app.fUseLoger();
+
 app.fUseProduct();
+app.fUseProductTag();
+app.fUseProductCategory();
+
 app.fStart(); // Запускаем приложение
