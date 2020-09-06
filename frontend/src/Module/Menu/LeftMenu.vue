52ccd96ac8c83792b13b5d5dc8975a8d24cbae09
alex <AAnPolozov@sberbank.ru>
2020-09-06 23:45:05 +0300

    icons menu

--- frontend/src/Module/Menu/LeftMenu.vue
+++ frontend/src/Module/Menu/LeftMenu.vue
@@ -1,22 +1,30 @@
 <template>
     <div class="left-menu">
         <div class="menu-group">
-            <router-link :to="'/' + sProductR" class="h1-item">Торговля</router-link>
+            <router-link :to="'/' + sProductR" class="h1-item">
+                <i class="fas fa-hands-helping"></i> Торговля
+            </router-link>
             <router-link :to="'/' + sProductR + '/add'" class="h2-item">Продажи</router-link>
             <router-link :to="'/'+ sProductCategoryR" class="h2-item">Закупки</router-link>
         </div>
         <div class="menu-group">
-            <router-link :to="'/' + sProductR" class="h1-item">Финансы</router-link>
+            <router-link :to="'/' + sProductR" class="h1-item">
+                <i class="fas fa-money-bill-wave"></i> Финансы
+            </router-link>
             <router-link :to="'/' + sProductR + '/add'" class="h2-item">Поступления</router-link>
             <router-link :to="'/'+ sProductCategoryR" class="h2-item">Расходы</router-link>
         </div>
         <div class="menu-group">
-            <router-link :to="'/' + sProductR" class="h1-item">Склады</router-link>
+            <router-link :to="'/' + sProductR" class="h1-item">
+                <i class="fas fa-people-carry"></i> Склады
+            </router-link>
             <router-link :to="'/' + sProductR + '/add'" class="h2-item">Главный склад</router-link>
             <router-link :to="'/'+ sProductCategoryR" class="h2-item">Еще один склад</router-link>
         </div>
         <div class="menu-group">
-            <router-link :to="'/' + sProductR" class="h1-item">Товары</router-link>
+            <router-link :to="'/' + sProductR" class="h1-item">
+                <i class="fas fa-boxes"></i> Товары
+            </router-link>
             <router-link :to="'/' + sProductR + '/add'" class="h2-item">Добавить</router-link>
             <router-link :to="'/'+ sProductCategoryR" class="h2-item">Категории</router-link>
         </div>
@@ -25,11 +33,11 @@
 
 <script lang='ts'>
 /**
- * Меню слева 
+ * Меню слева
  */
 
-import { sRoute as sProductR  } from "../../../../Entity/Routes/ProductR";
-import { sRoute as sProductCategoryR  } from "../../../../Entity/Routes/ProductCategoryR";
+import { sRoute as sProductR } from "../../../../Entity/Routes/ProductR";
+import { sRoute as sProductCategoryR } from "../../../../Entity/Routes/ProductCategoryR";
 import { Component, Prop, Vue } from "vue-property-decorator";
 @Component({
     name: "LeftMenu",
